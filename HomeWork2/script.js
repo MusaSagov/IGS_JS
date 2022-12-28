58a457ff3a9c2c138b72cca699dcf86fd6ac12a2
MusaSagov <sagov82@mail.ru>
2022-12-28 23:46:10 +0300

    HomeWork2

---
+++ HomeWork2/script.js
@@ -0,0 +1,12 @@
+"use strict"
+let name = prompt("Как Вас зовут?")
+console.log(name);
+
+let num = prompt("Введите число")
+if (num>0) {
+  alert(1);
+}else if (num<0){
+  alert(-1)
+}else{
+  alert(0)
+}
